1f0363ec3d3c0f48d32af7354138f7d3d7ebcd51
Syed Haider Ali <hamzakhalil@joyn.com.pk>
2023-09-28 11:06:09 +0500

    Finalized

---
+++ base/controller.py
@@ -0,0 +1,131 @@
+import threading
+from django.core.mail import send_mail
+from django.utils import timezone
+from rest_framework.response import Response
+from django.contrib.auth import authenticate
+from django.contrib.auth.hashers import make_password
+
+from user_management.settings import EMAIL_HOST_USER
+from base.serializer import *
+from base.models import Token, User
+from utils.reusable_methods import get_first_error_message, generate_six_length_random_number
+from utils.response_messages import *
+from utils.helper import create_response, paginate_data
+
+
+class ChangePasswordController:
+    serializer_class = ChangePasswordSerializer
+
+    def change_password(self, request):
+        user = request.user
+        if not user:
+            return create_response({}, USER_NOT_FOUND, status_code=400)
+
+        serialized_data = self.serializer_class(data=request.data, context={'user': user})
+
+        if serialized_data.is_valid():
+            user.set_password(request.data['new_password'])
+            user.save()
+            return create_response({}, PASSWORD_UPDATED, status_code=200)
+        else:
+            return create_response({}, get_first_error_message(serialized_data.errors, UNSUCCESSFUL), status_code=400)
+
+
+class VerifyOtpController:
+    serializer_class = VerifyOtpSerializer
+
+    def verify_otp(self, request):
+        # check OTP time delay
+        time_delay = timezone.now() - timezone.timedelta(seconds=300)
+        user = User.objects.filter(otp=request.data.get("otp"), otp_generated_at__gt=time_delay).first()
+
+        if not user:
+            return create_response({}, INVALID_OTP, status_code=400)
+
+        serialized_data = self.serializer_class(data=request.data, context={'user': user})
+
+        if serialized_data.is_valid():
+            user.set_password(request.data['new_password'])
+            user.otp = None
+            user.save()
+            return create_response({}, SUCCESSFUL, status_code=200)
+        else:
+            return create_response({}, get_first_error_message(serialized_data.errors, UNSUCCESSFUL), status_code=400)
+
+
+class ForgetPasswordController:
+    serializer_class = ForgetPasswordSerializer
+
+    def forget_password(self, request):
+        serialized_data = self.serializer_class(data=request.data)
+        if not serialized_data.is_valid():
+            return create_response({}, get_first_error_message(serialized_data.errors, UNSUCCESSFUL), status_code=400)
+
+        user = User.objects.filter(email=request.data['email']).first()
+        if not user:
+            return create_response({}, USER_NOT_FOUND, status_code=404)
+
+        otp = generate_six_length_random_number()
+        user.otp = otp
+        user.otp_generated_at = timezone.now()
+        user.save()
+        subject = "Password Recovery Request"
+        message = f"""
+            Hi {user.get_full_name()},
+            Your request for password recovery has been received.
+            Please use the following otp.
+            OTP: {otp}
+            """
+        recipient_list = [request.data.get("email")]
+        t = threading.Thread(target=send_mail, args=(subject, message, EMAIL_HOST_USER, recipient_list))
+        t.start()
+        return create_response({}, EMAIL_SUCCESSFULLY_SENT, status_code=200)
+
+
+class RegisterController:
+    serializer_class = UserSerializer
+
+    def create(self, request):
+        serialized_data = self.serializer_class(data=request.data)
+
+        if serialized_data.is_valid():
+            instance = serialized_data.save()
+            return create_response(self.serializer_class(instance).data, SUCCESSFUL, status_code=200)
+        else:
+            return create_response({}, get_first_error_message(serialized_data.errors, UNSUCCESSFUL), status_code=400)
+
+
+class LoginController:
+    serializer_class = LoginSerializer
+
+    def login(self, request):
+        serialized_data = self.serializer_class(data=request.data)
+
+        if not serialized_data.is_valid():
+            return Response({'error': get_first_error_message(serialized_data.errors, UNSUCCESSFUL)}, status=400)
+
+        user = authenticate(username=request.data['username'], password=request.data['password'])
+        if not user:
+            return create_response({}, message=INCORRECT_EMAIL_OR_PASSWORD, status_code=400)
+
+        response_data = {
+            "token": user.get_access_token(),
+            "name": user.get_full_name(),
+            "username": user.username,
+        }
+        Token.objects.update_or_create(defaults={"token": response_data.get("token")}, user_id=user.id)
+        user.failed_login_attempts = 0
+        user.last_failed_time = None
+        user.last_login = timezone.now()
+        user.save()
+        return create_response(response_data, SUCCESSFUL, status_code=200)
+
+
+class LogoutController:
+    def logout(self, request):
+        user = request.user.id
+        token = Token.objects.filter(user=user)
+        if not token:
+            return create_response({}, UNSUCCESSFUL, status_code=400)
+        token.delete()
+        return create_response({}, SUCCESSFUL, status_code=200)
--- base/views.py
+++ base/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
-# Create your views here.
+from utils.base_authentication import JWTAuthentication
+from base.controller import *
+
+register_controller = RegisterController()
+login_controller = LoginController()
+logout_controller = LogoutController()
+forget_password_controller = ForgetPasswordController()
+verify_otp_controller = VerifyOtpController()
+change_password_controller = ChangePasswordController()
+
+
+class RegisterAPIView(ModelViewSet):
+    def create(self, request):
+        return register_controller.create(request)
+
+
+class LoginAPIView(ModelViewSet):
+    def login(self, request):
+        return login_controller.login(request)
+
+
+class LogoutAPIView(ModelViewSet):
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def logout(self, request):
+        return logout_controller.logout(request)
+
+
+class ChangePasswordAPIView(ModelViewSet):
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request):
+        return change_password_controller.change_password(request)
+
+
+class VerifyOtpAPIView(ModelViewSet):
+    def post(self, request):
+        return verify_otp_controller.verify_otp(request)
+
+
+class ForgetPasswordAPIView(ModelViewSet):
+    def post(self, request):
+        return forget_password_controller.forget_password(request)
+
+
+
